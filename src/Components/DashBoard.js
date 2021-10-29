f43bebd6b4e6274414c383f44df5968fe7169313
Robin Kelsick II <robinkelsickii@Mollies-MacBook-Pro.local>
2021-10-28 21:43:40 -0400

    waiting for API fix

--- src/Components/DashBoard.js
+++ src/Components/DashBoard.js
@@ -5,25 +5,25 @@ import { Route } from "react-router";
 
 export default function Dashboard() {
 
-//     axios.get('https://port-3000-aincbootcampapi-ianrios529550.codeanyapp.com/api/auth/user'), {
+    axios.get('https://port-3000-aincbootcampapi-ianrios529550.codeanyapp.com/api/auth/user'), {
         
-//     }
-//     .then(function (response) {
-//       console.log(response);
-//     })
-//     .catch(function (error) {
-//       console.log(error);
-//     })
+    }
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
 
-//     return (
-//         <Route>
-//             <Container>
-//                 <Row>
-//                     <Col>
-//                         <h1>Dashboard</h1>
-//                     </Col>
-//                 </Row>
-//             </Container>
-//         </Route>
-//     )
+    return (
+        <Route>
+            <Container>
+                <Row>
+                    <Col>
+                        <h1>Dashboard</h1>
+                    </Col>
+                </Row>
+            </Container>
+        </Route>
+    )
 }
