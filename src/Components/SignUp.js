4d5ce66fd506c3f2e2a49cee7072776682b11f70
Robin Kelsick II <robinkelsickii@Mollies-MacBook-Pro.local>
2021-10-28 21:42:22 -0400

    waiting for API fix

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -7,7 +7,9 @@ import { useHistory } from "react-router";
 
 export default function SignUp(props) {
     const [data, setData] = useState({});
+    const [validated, setValidated] = useState(false);
     const history = useHistory()
+    const noText = 'This field cannot be left empty'
 
 
 
@@ -23,26 +25,34 @@ export default function SignUp(props) {
         console.log('User Created')
         console.log(data)
         // run axios call sending all the user data
-        axios.post('https://port-3000-aincbootcampapi-ianrios529550.codeanyapp.com/api/auth/register', {
+        axios.post('https://port-3000-aincbootcampapi-ianrios529550.preview.codeanywhere.com//api/auth/register', {
             name: data.firstName + " " + data.lastName,
             email: data.email,
             password: data.password,
         })
-        .then(function (response) {            
-            console.log(response);
-            // save data to localstorage
-            const token = response.data.data.token
-            localStorage.setItem('token', token)
-            console.log(token)
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+            .then(function (response) {
+                console.log(response);
+                // save data to localstorage
+                const token = response.data.data.token
+                localStorage.setItem('token', token)
+                console.log(token)
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+        const form = e.currentTarget;
+        if (form.checkValidity() === false) {
+            e.preventDefault();
+            e.stopPropagation();
+        }
+        setValidated(true);
+
     }
     const loginBtn = () => {
         history.push('/Login')
     }
-    
+
     return (
         <Container className='text-center'>
             <Row>
@@ -51,31 +61,49 @@ export default function SignUp(props) {
                 </Col>
             </Row>
             <Row>
-                <Form onSubmit={handleSubmit}>
+                <Form noValidate validated={validated} onSubmit={handleSubmit}>
                     <Col className='pt-5 text-center'>
                         <Form.Group onChange={handleChange} value={data.firstName || ""}>
                             <Form.Label >First Name</Form.Label>
-                            <Form.Control id='firstName' type="text" placeholder="Enter First Name" />
+                            <Form.Control required id='firstName' type="text" placeholder="Enter First Name" />
+                            <Form.Control.Feedback>Thanks!</Form.Control.Feedback>
+                            <Form.Control.Feedback type='invalid'>{noText}</Form.Control.Feedback>
                         </Form.Group>
 
                         <Form.Group onChange={handleChange} value={data.lastName || ""}>
                             <Form.Label>Last Name</Form.Label>
-                            <Form.Control id='lastName' type="text" placeholder="Enter Last Name" />
+                            <Form.Control required id='lastName' type="text" placeholder="Enter Last Name" />
+                            <Form.Control.Feedback>Thanks!</Form.Control.Feedback>
+                            <Form.Control.Feedback type='invalid'>{noText}</Form.Control.Feedback>
                         </Form.Group>
 
                         <Form.Group onChange={handleChange} value={data.email || ""}>
                             <Form.Label>E-Mail</Form.Label>
-                            <Form.Control id='email' type="text" placeholder="example@website.com" />
+                            <Form.Control required id='email' type="text" placeholder="example@website.com" />
+                            <Form.Control.Feedback>Thanks!</Form.Control.Feedback>
+                            <Form.Control.Feedback type='invalid'>{noText}</Form.Control.Feedback>
                         </Form.Group>
 
                         <Form.Group onChange={handleChange} value={data.password || ""}>
                             <Form.Label>Password</Form.Label>
-                            <Form.Control id='password' type="text" placeholder="Create a password" />
+                            <Form.Control required id='password' type="text" placeholder="Create a password" />
+                            <Form.Control.Feedback>Thanks!</Form.Control.Feedback>
+                            <Form.Control.Feedback type='invalid'>{noText}</Form.Control.Feedback>
                         </Form.Group>
+
+                        <Form.Group className="mt-3">
+                            <Form.Check
+                                required
+                                label="Agree to terms and conditions"
+                                feedback="You must agree before submitting."
+                                feedbackType="invalid"
+                            />
+                        </Form.Group>
+
                         <Button type='submit' className='btn btn-primary mt-3 mb-3'>Sign Up</Button>
                         <div>
-                        <p> Already Have an account? Sign In!</p>
-                        <Button type='button' className='btn btn-success' onClick={loginBtn}>Login</Button>
+                            <p> Already Have an account? Sign In!</p>
+                            <Button type='button' className='btn btn-success' onClick={loginBtn}>Login</Button>
                         </div>
                     </Col>
                 </Form>
